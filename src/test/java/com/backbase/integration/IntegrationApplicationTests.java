076f17177629049914ad596ad0ac641615c90dcc
Matheus Mendes <matheusrochamendes@gmail.com>
2018-11-23 18:04:38 -0200

    Unit tests created for the address spring endpoint

--- src/test/java/com/backbase/integration/IntegrationApplicationTests.java
+++ src/test/java/com/backbase/integration/IntegrationApplicationTests.java
@@ -1,16 +1,37 @@
 package com.backbase.integration;
 
+import com.backbase.integration.endpoint.AddressEndpoint;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(AddressEndpoint.class)
+@AutoConfigureWebClient
 public class IntegrationApplicationTests {
 
-	@Test
-	public void contextLoads() {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+	public void should_return_the_address_and_200() throws Exception {
+        String address = "pretty tested";
+        mvc.perform(get("/address?address="+address)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("address", Matchers.is(address)))
+                .andExpect(status().isOk());
+
 	}
 
 }
